ad986253b27c66bdeaa1287291e017fb01da954f
hanahassan <hana0hassan0@gmail.com>
2024-07-28 11:48:42 +0300

    Nav Bar done

--- src/Pages/NavBar.js
+++ src/Pages/NavBar.js
@@ -1,22 +1,22 @@
 import React from "react";
 import { NavLink } from 'react-router-dom';
-import '../NavBar.css';
+import '../index.css'; // Make sure this path is correct
 
 function NavBar() {
     return (
         <header className="header">
             <div className="container">
                 <NavLink className="brand" to='/'>PoolPal</NavLink>
-                <input className="toggle-input" type="checkbox" id="navbar-open"/>
-                <label className="toggle-label" htmlFor="navbar-open">    
+                <input className="toggle-input" type="checkbox" id="navbar-open" />
+                <label className="toggle-label" htmlFor="navbar-open">
                     <svg xmlns="http://www.w3.org/2000/svg" className="icon" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M4 6h16M4 12h16M4 18h16" />
                     </svg>
                 </label>
                 <nav className="nav">
                     <ul className="nav-list">
-                        <li><NavLink className={({ isActive }) => isActive ? 'active' : 'inactive'} to='/'>Home </NavLink></li>
-                        <li><NavLink className={({ isActive }) => isActive ? 'active' : 'inactive'} to='/CarOption'>Find Pool Buddy </NavLink></li>
+                        <li><NavLink className={({ isActive }) => isActive ? 'active' : 'inactive'} to='/'>Home</NavLink></li>
+                        <li><NavLink className={({ isActive }) => isActive ? 'active' : 'inactive'} to='/CarOption'>Find Pool Buddy</NavLink></li>
                         <li className="signup"><NavLink className={({ isActive }) => isActive ? 'signup-active' : 'signup-inactive'} to='/SignUp'>SignUp</NavLink></li>
                     </ul>
                 </nav>
