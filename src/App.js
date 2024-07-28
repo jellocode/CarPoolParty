c3c346c144140b9b8ad848a5e0bab166dd8da11b
hanahassan <hana0hassan0@gmail.com>
2024-07-28 14:00:50 +0300

    Pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
 import './index.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import CarOption from './pages/CarOption';
-import Form from './pages/Form';
-import LandingPage from './pages/LandingPage';
-import SignUp from './pages/SignUp';
+import CarOption from './Pages/CarOption';
+import Form from './Pages/Form';
+import LandingPage from './Pages/LandingPage';
+import SignUp from './Pages/SignUp';
 
 function App() {
   return (
--- src/Pages/LandingPage.js
+++ src/Pages/LandingPage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import NavBar from '../pages/NavBar';
+import NavBar from '../Pages/NavBar';
 
 function LandingPage() {
     return (
